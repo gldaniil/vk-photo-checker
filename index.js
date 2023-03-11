f449255ca200233f63c517f61b36ed37557a51c6
gldaniil <daniil.gajfullin@yandex.ru>
2023-03-11 16:22:12 +0500

    Перечень команд бота

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const TelegramApi = require("node-telegram-bot-api");
 
 const bot = new TelegramApi(cfg.tokenTlg, { polling: true });
 
-bot.on("message", (msg) => {
-  const chat = msg.chat;
-  const text = msg.text;
-  
-  chat.id == cfg.chatId
-    ? bot.sendMessage(chat.id, `${text}`)
-    : chat.last_name != undefined
-    ? bot.sendMessage(
-        cfg.chatId,
-        `${chat.first_name} ${chat.last_name} (${chat.id}): ${text}`
-      )
-    : bot.sendMessage(cfg.chatId, `${chat.first_name} (${chat.id}): ${text}`)
-})
+const startBot = () => {
+  bot.setMyCommands([
+    { command: "/start", description: "Начало работы" },
+    { command: "/add", description: "Добавить пользователя в список" },
+    { command: "/delete", description: "Убрать пользователя из списка" },
+    { command: "/info", description: "Получить текущий спиок пользователей" },
+  ]);
+
+  bot.on("message", async (msg) => {
+    const chat = msg.chat;
+    const text = msg.text;
+
+    chat.id == cfg.chatId
+      ? await bot.sendMessage(chat.id, `${text}`)
+      : chat.last_name != undefined
+      ? await bot.sendMessage(
+          cfg.chatId,
+          `${chat.first_name} ${chat.last_name} (${chat.id}): ${text}`
+        )
+      : await bot.sendMessage(
+          cfg.chatId,
+          `${chat.first_name} (${chat.id}): ${text}`
+        );
+  });
+};
