fd7d92af743242fb0710f5a816a8545740f640c2
gldaniil <daniil.gajfullin@yandex.ru>
2023-03-13 00:36:30 +0500

    Завершенная часть

--- models.js
+++ models.js
@@ -1,9 +1,18 @@
-const sequelize = require("./db");
-const { DataTypes } = require("sequelize");
+const sequelize = require('./db');
+const { DataTypes } = require('sequelize');
 
 const User = sequelize.define("user", {
   id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
   userId: { type: DataTypes.STRING, unique: true }
 });
 
-module.exports = User
+const Photo = sequelize.define("photo", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
+  ownerId: { type: DataTypes.STRING },
+  photoId: { type: DataTypes.STRING, unique: true }
+});
+
+module.exports = {
+  User,
+  Photo,
+};
